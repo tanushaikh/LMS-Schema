3b64d6622ad314b520e4ca29b10793594c2a63fd
tanushaikh <114586192+tanushaikh@users.noreply.github.com>
2025-09-11 23:01:14 +0530

    update session and shedule slug

--- session_lms/serializers.py
+++ session_lms/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import Session
 
 class SessionSerializer(serializers.ModelSerializer):
+    slug = serializers.ReadOnlyField()  # backend generate karega
+    user = serializers.PrimaryKeyRelatedField(read_only=True)  # frontend se mat bhejna
+
     class Meta:
         model = Session
-        fields = "__all__"
+        fields = '__all__'
+        extra_kwargs = {
+            "user": {"required": False, "allow_null": True},
+            "slug": {"read_only": True},
+        }
